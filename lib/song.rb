10adf5c1686237750fa26befe48fd76d65d3aa41
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-25 19:32:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,20 @@
 class Song 
-  attr_accessor :title, :artist 
+  attr_accessor :name, :artist 
   
   @@all = []
   
   def initialize(title)
-    @title = title
+    @name = name
     
     @@all << self 
   end 
   
   def self.all 
     @@all 
+  end 
+  
+  def artist_name
+    self.artist.name 
   end 
   
 end 
