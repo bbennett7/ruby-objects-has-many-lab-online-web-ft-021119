2ff250e90f97c384bd80dbfdb37e50eb8566ebd4
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-25 19:51:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,20 @@
 class Author 
   attr_accessor :name 
-  
+
   def initialize(name)
     @name = name 
+    
+    @posts = []
+  end 
+  
+  def posts 
+    @posts 
+  end 
+  
+  def add_post(post)
+    post = Post.new 
+    @posts << post 
+    post.Author = self 
   end 
   
 end 
