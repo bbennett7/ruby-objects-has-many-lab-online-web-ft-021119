e2b3ee92fca885fe6905ef91b167ddd2573b07b6
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-25 20:02:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,16 +13,23 @@ class Author
     @posts 
   end 
   
-  def add_post(post)
+  def track_posts 
     @posts << post 
     @@all_posts << post 
+  end 
+  
+  def add_post(post)
+    #@posts << post 
+    #@@all_posts << post
+    track_posts 
     post.author = self 
   end 
   
   def add_post_by_title(post)
     post = Post.new(post)
-    @posts << post 
-    @@all_posts << post 
+    #@posts << post 
+    #@@all_posts << post
+    track_posts 
     post.author = self 
   end 
   
